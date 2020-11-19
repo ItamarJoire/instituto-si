67b82b520c0eec72c3e3f50ea4137537ba33baa9
Itamar Joire <30444253+ItamarJoire@users.noreply.github.com>
2020-11-19 08:00:47 -0300

    js

--- public/scripts/page-students.js
+++ public/scripts/page-students.js
@@ -1,5 +1,22 @@
-function addNewField(){
-    const createOneStudent = document.querySelector('.create-one-student');
+// const listElement = document.querySelector('button .excluir');
 
-    
+function alerta() {
+    alert("Tem certeza que quer excluir a tabela?");
+  }
+
+function confirma(){
+  confirm("Certeza que quer excluir?");
+}
+
+function exibe(){
+  var valor = prompt("Quantas linhas deseja adicionar?");
+
+  if(valor != null){
+    alert("Linha " + valor + " adcionada com sucesso!");
+  
+  }
+}
+
+function submitForm(){
+  alert('CPF e SENHA corretos!');
 }
